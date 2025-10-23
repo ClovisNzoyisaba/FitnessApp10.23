ca7ee250fdd620e2af7d2fe91c265fc4c68ab316
ClovisNzoyisaba <clovisnzoyisaba@gmail.com>
2025-10-23 17:54:40 -0500

    initialized with xcode

---
+++ FitnessApp10.23/FitnessApp10.23/FitnessApp10_23App.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessApp10_23App.swift
+//  FitnessApp10.23
+//
+//  Created by Clovis Nzoyisaba on 10/23/25.
+//
+
+import SwiftUI
+
+@main
+struct FitnessApp10_23App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
